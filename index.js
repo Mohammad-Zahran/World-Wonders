c15d4da3b92e9826e0da27848c72a6985734a4f8
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-02 01:53:58 +0200

    Creating the img element

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-
 fetchData();
+
 async function fetchData() {
-    try{
-        const worldCards = document.getElementById("world-cards"); // Get the main container
+    try {
         const response = await fetch("https://www.world-wonders-api.org/v0/wonders");
 
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error("Could not fetch resource");
         }
 
-        const data = await response.json()
-       
+        const data = await response.json();
+        const worldCards = document.getElementById("card"); 
+
+        // What I am doing is creating new elements for each data inside the API 
         data.forEach(element => {
             const worldName = document.createElement("h1");
-            worldName.textContent = element.name
-            worldCards.appendChild(worldName)
+            worldName.textContent = element.name;
+
+            const worldImage = document.createElement("img");
+            worldImage.src = element.links.images[0]; 
+            worldImage.alt = element.name; 
+
+            worldCards.appendChild(worldName);
+            worldCards.appendChild(worldImage);
         });
 
-       
-
-    }
-    catch(error){
-        console.log(error)
+    } catch (error) {
+        console.error(error);
     }
 }
-
