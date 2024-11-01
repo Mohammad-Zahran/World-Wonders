d46796c541a63500fa532e2096e9a0ac96d1540c
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-02 00:58:36 +0200

    Creating the h1 elements for the name of worlds

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 
-
+fetchData();
 async function fetchData() {
     try{
+        const worldCards = document.getElementById("world-cards"); // Get the main container
         const response = await fetch("https://www.world-wonders-api.org/v0/wonders");
 
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
         const data = await response.json()
-        console.log(data);
+       
+        data.forEach(element => {
+            const worldName = document.createElement("h1");
+            worldName.textContent = element.name
+            worldCards.appendChild(worldName)
+        });
+
+       
 
     }
     catch(error){
@@ -13,4 +25,3 @@ async function fetchData() {
     }
 }
 
-fetchData()
